baa7af5546a4b807d70a57582fe30cb0bce86944
Hernando-s <hernando.sousa13@gmail.com>
2023-10-08 01:24:19 -0300

    consulta query simples

--- src/main/java/spring/Repository/PostRepository.java
+++ src/main/java/spring/Repository/PostRepository.java
@@ -1,5 +1,7 @@
 package spring.Repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import spring.domain.Post;
@@ -8,7 +10,7 @@ import spring.domain.Post;
  
 public interface PostRepository extends MongoRepository<Post, Long>{
 
-	
+  List<Post> findByTitleContaining(String text);
 	
 
 }
--- src/main/java/spring/Resource/PostResource.java
+++ src/main/java/spring/Resource/PostResource.java
@@ -1,10 +1,13 @@
 package spring.Resource;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import spring.Service.PostService;
@@ -25,7 +28,13 @@ public class PostResource {
     	return ResponseEntity.ok().body(obj);
     }
     
-    
+    @GetMapping(value = "/titlesearch")
+    public ResponseEntity<List<Post>> finByTitle(@RequestParam (value = "text" , defaultValue = "")String text){
+    	text = URL.decodeParam(text);
+    	
+    	List<Post> list =service.findByTitle(text);
+    	return ResponseEntity.ok().body(list);
+    }
     
  
 		
