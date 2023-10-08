1c3207185834840d764fec17c5787b17fdd0a1c4
Hernando-s <hernando.sousa13@gmail.com>
2023-10-08 00:08:53 -0300

    postservice e postrepository

---
+++ src/main/java/spring/Resource/PostResource.java
@@ -0,0 +1,33 @@
+package spring.Resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import spring.Service.PostService;
+import spring.domain.Post;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+    
+	@Autowired	
+	private PostService service;
+	
+  
+    
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Post> finById(@PathVariable Long id){
+    	Post obj = service.findById(id);
+    	return ResponseEntity.ok().body(obj);
+    }
+    
+    
+    
+ 
+		
+	}
+
---
+++ src/main/java/spring/Service/PostService.java
@@ -0,0 +1,27 @@
+package spring.Service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import spring.Repository.PostRepository;
+import spring.Service.exception.ObjectNotFoundException;
+import spring.domain.Post;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	
+	
+	public Post findById(Long id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
+		
+
+
+}
